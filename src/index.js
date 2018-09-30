1c0a7650169a01e2154d3cfec53ca0717be9745e
Elena Khamitsevitch <elena_khamitsevitch@mail.ru>
2018-09-30 19:04:11 +0300

    6

--- src/index.js
+++ src/index.js
@@ -1,3 +1,24 @@
-module.exports = function check(str, bracketsConfig) {
-  // your solution
-}
+module.exports = function check(a, bracketsConfig)  {
+  var x = [];
+    for (var i=0, len=bracketsConfig.length; i<len; ++i){
+      for(var p = 0, l = a.length; p < l; p++){
+      if (a[p] === bracketsConfig[i][0]){
+        x.push(a[p]);
+      }
+      if (a[p] === bracketsConfig[i][1]){
+        if (x.length === 0){
+          return false;
+         } else if(a[p]===bracketsConfig[i][1]){
+        x.pop();
+      }
+      else{
+          return false;
+         }
+    }}
+        if (x.length === 0) {    
+      return true;
+    }
+   else {
+     return false;
+  }}}
+  
